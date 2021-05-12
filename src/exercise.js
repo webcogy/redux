4a15205a6b810d32963b50027e30f8c56405b349
webcogy <ungk456@naver.com>
2021-05-12 12:29:51 +0900

    6-3 리덕스 사용할 준비하기

--- src/exercise.js
+++ src/exercise.js
@@ -57,6 +57,24 @@ function reducer(state = initialState, action){
             return state; // 기본 값 설정. 파라미터가 undefiend인 경우를 대비
     }
 }
-
+// 스토어 생성
 const store = createStore(reducer);
 console.log(store.getState());
+
+const listener = () => {
+    const state = store.getState();
+    console.log(state);
+}
+
+// 구독을 해줘야 store를 사용할 수 있게 된다.
+const unsubscribe = store.subscribe(listener);
+
+// dispatch할 때 마다 store가 호출됨
+store.dispatch(increase());
+store.dispatch(decrease());
+store.dispatch(changeText('안녕하세요'));
+store.dispatch(addToList({id:1, text:'ㅎㅎㅎ'}));
+
+// window객체로 사용할 수 있게 설정, console에서 dispatch등등 사용해볼 수 있게 됨
+window.store = store;
+window.unsubscribe = unsubscribe ; // unsubscribe(); 사용시 store 구독취소됨
